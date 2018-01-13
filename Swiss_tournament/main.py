e7a1c84e539228f9235659c5463a52c05ca169b7
Jorgenssen <n.tatyannikov@gmail.com>
2018-01-13 17:34:51 +0100

    formatting on main

--- Swiss_tournament/main.py
+++ Swiss_tournament/main.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python
+#-------------------------------------------------------------------------------------
+#             FILE: fencer.py
+#      DESCRIPTION: class module providing the base fencer entity
+#           AUTHOR: Nikita Tatiannikov, n.tatyannikov@gmail.com
+#          COMPANY: Tramazzone
+#          VERSION: 0.00
+# VERSIONS HISTORY: 2018-01-11 (0.0) - to be developed
+#-------------------------------------------------------------------------------------
+
+
 import fencer

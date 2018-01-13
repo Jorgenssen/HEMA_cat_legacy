ff9ccc907761e1152f8234bf05c8b59f0beb183d
Jorgenssen <n.tatyannikov@gmail.com>
2018-01-13 17:50:35 +0100

    fencer ID was added to inputs.py

--- Swiss_tournament/inputs.py
+++ Swiss_tournament/inputs.py
@@ -4,9 +4,10 @@
 #      DESCRIPTION: preparing of raw data about fencers
 #           AUTHOR: Nikita Tatiannikov, n.tatyannikov@gmail.com
 #          COMPANY: Tramazzone
-#          VERSION: 0.01
-# VERSIONS HISTORY: 2018-01-11 - base logic of reading to lists,
-#                                transfering to list of objects
+#          VERSION: 0.02
+# VERSIONS HISTORY: 2018-01-11 (0.01) - base logic of reading to lists,
+#                                       transfering to list of objects
+#                   2018-01-13 (0.02) - fencer ID was added as unique attribute (key)
 #-------------------------------------------------------------------------------------
 
 #импорт базовых модулей
@@ -37,7 +38,7 @@ with open(input_file, "r", newline="") as file:
 print('\n',index,'\n')
 
 #Проверка атрибутов объектов
-print('name', 'club', 'wins', 'defeats', 'hits_got', 'hits_given')
+print('ID', 'name', 'club', 'wins', 'defeats', 'hits_got', 'hits_given')
 for fencer in index:
-    print(fencer.name, ' - ', fencer.club, ' - ', fencer.wins, ' - ', fencer.defeats, ' - ', fencer.hits_got, ' - ', fencer.hits_given)
+    print(fencer.ID, ' - ', fencer.name, ' - ', fencer.club, ' - ', fencer.wins, ' - ', fencer.defeats, ' - ', fencer.hits_got, ' - ', fencer.hits_given)
 

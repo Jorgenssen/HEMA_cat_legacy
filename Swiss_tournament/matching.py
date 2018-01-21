c3c10e14b23ab23247f0ce5e796374394cc25893
Jorgenssen <n.tatyannikov@gmail.com>
2018-01-21 18:54:24 +0100

    dependency bug was fixed

--- Swiss_tournament/matching.py
+++ Swiss_tournament/matching.py
@@ -16,23 +16,21 @@ import logging.config
 
 #импорт внутренних модулей
 from fencer import Fencer
-from main import index
 
 #выбираем лог-конфиг и логгера
 logging.config.fileConfig('log.conf')
 logger = logging.getLogger("Matching") 
-
-#инициализируем рандомайзер в диапазоне числа наших драчунов
-random.randint(1, len(index))
 
 #инициализируем необходимые переменные
 matched = []
 
 #магическая функция, которая разбивает наших драчунов на пары
 def matching(index):
+    #инициализируем рандомайзер в диапазоне числа наших драчунов
+    random.randint(1, len(index))
     not_matched = []
     opponent_ID = 0
-
+    logger.info('Initial matching...')
     for fencer in index:
         not_matched.append(int(fencer.ID))
 
@@ -45,22 +43,39 @@ def matching(index):
 
     if matched:
         matched.append((not_matched[0],'auto_win'))
-                            
+    logger.info('Initial matching finished')
+    table = PrettyTable(['№','Name 1', 'Name 2'])
+    for i in range(len(matched)):
+        '''
+        index[matched_fe[i]-1].name/index[matched_op[i]-1].name - здесь мы обращаемся
+        к атрибуту name экземпляра fencer по matched_fe[i]-1,
+        где matched_fe[i] - это интовое значение fencer.ID, которое у нас на 1 больше,
+        чем порядковый индекс этого же экземпляра в index'e
+        '''
+        try:
+            table.add_row([i+1,index[matched[i][0]-1].name, index[matched[i][1]-1].name])
+        except TypeError as err:
+            table.add_row([i+1,index[matched[i][0]-1].name, 'auto win'])
+   
+    print(table)
+                          
 #проверка списка
 print(matched)
 
+'''
 #вывод пар
 table = PrettyTable(['№','Name 1', 'Name 2'])
 for i in range(len(matched)):
-    '''
+    
     index[matched_fe[i]-1].name/index[matched_op[i]-1].name - здесь мы обращаемся
     к атрибуту name экземпляра fencer по matched_fe[i]-1,
     где matched_fe[i] - это интовое значение fencer.ID, которое у нас на 1 больше,
     чем порядковый индекс этого же экземпляра в index'e
-    '''
+    
     try:
         table.add_row([i+1,index[matched[i][0]-1].name, index[matched[i][1]-1].name])
     except TypeError as err:
         table.add_row([i+1,index[matched[i][0]-1].name, 'auto win'])
    
 print(table)
+'''
